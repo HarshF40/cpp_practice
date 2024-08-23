bedd158c183f8f73b6a2519e3a2d1a287ecb4e00
HarshF40 <harshgaonkar57@gmail.com>
2024-08-23 20:51:34 +0530

    add book already exists feature

--- library_sys/class_Books.cpp
+++ library_sys/class_Books.cpp
@@ -13,6 +13,7 @@ Books::Books(){
 
    int book_counter=1;
    std::string str;
+   std::string book_name;
 
       Btemp.list_books();
 
@@ -60,13 +61,24 @@ Books::Books(){
 
          Btemp.list_books();
 
+loop2:
 
           curr = new book;
 
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
 
         std::cout<<"\nEnter The Name of the book "<<i+1<<": ";
-        std::getline(std::cin,curr->Name);
+        std::getline(std::cin,book_name);
+
+        for(book* i = start;i!=nullptr;i=i->next){
+          if(to_lower_string(i->Name) == to_lower_string(book_name)){
+            std::cout<<"\nBook Already Exists!\n\n\n";
+            goto loop2;
+          }
+        }
+
+        curr->Name = book_name;
+
 
         std::cout<<"Enter Author's name: ";
         std::getline(std::cin,curr->Author);
@@ -115,6 +127,8 @@ Books::Books(){
           int i=0;
           do{
 
+loop3:
+
             std::string str;
 
              curr = new book;
@@ -122,7 +136,16 @@ Books::Books(){
              std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
 
         std::cout<<"\nEnter The Name of the book "<<i+1<<": ";
-        std::getline(std::cin,curr->Name);
+        std::getline(std::cin,book_name);
+
+        for(book* i = start;i!=nullptr;i=i->next){
+          if(to_lower_string(i->Name) == to_lower_string(book_name)){
+            std::cout<<"\nBook Already Exists!\n\n\n";
+            goto loop3;
+          }
+        }
+
+        curr->Name=book_name;
 
         std::cout<<"Enter Author's name: ";
         std::getline(std::cin,curr->Author);
