edf356b6a8229e08bac60f289da2fff41a36dabc
HarshF40 <harshgaonkar57@gmail.com>
2024-09-14 08:55:11 +0530

    learn mutex

---
+++ Practice/multithread/mutexPrac.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<thread>
+#include<mutex>
+#include<vector>
+
+static int shared_value = 0;
+std::mutex glock;
+
+//function used by each individual thread.
+void shared_value_increment(){
+ glock.lock();
+    std::cout<<"\nHello From: "<<std::this_thread::get_id();
+    shared_value++;
+ glock.unlock();
+}
+
+int main(){
+
+  std::vector<std::thread> threads;
+  
+  for(int i=0;i<1000;i++){
+    threads.push_back(std::thread(shared_value_increment));
+  }
+
+  for(int i=0;i<threads.size();i++){
+    threads[i].join();
+  }
+
+  std::cout<<"\nShared Value: "<<shared_value;
+
+  return 0;
+}
