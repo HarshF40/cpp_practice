56b3de4fd5a961b8a326cf8a3352d0ea7ce9a62c
HarshF40 <harshgaonkar57@gmail.com>
2024-09-17 16:40:29 +0530

    file sim complete

--- Practice/multithread/file_downloaod_sim.cpp
+++ Practice/multithread/file_downloaod_sim.cpp
@@ -4,28 +4,46 @@
 #include<chrono>
 #include<mutex>
 
+#define green "\033[32m"
+#define yellow "\x1b[33m"
+#define cyan "\033[36m"
+#define reset "\x1b[0m"
+
 std::mutex dlock,slock;
 
 std::random_device rd{};
 std::seed_seq ss{rd(),rd(),rd(),rd()};
 std::mt19937 mt{ss};
 std::uniform_int_distribution downTime{1,10};
-std::uniform_int_distribution prosTims{1,3};
+std::uniform_int_distribution prosTime{1,5};
+
+void process_file(int file_id){
+  std::unique_lock<std::mutex> lock(dlock);
+    int ptime = prosTime(mt);
+    std::cout<<cyan<<"Processing File "<<file_id<<"... : ("<<ptime<<" seconds)"<<reset<<std::endl;
+  lock.unlock();
+
+    std::this_thread::sleep_for(std::chrono::seconds(ptime));
+
+ std::unique_lock<std::mutex> lock2(slock);
+    std::cout<<green<<"File "<<file_id<<" processed successfully!"<<reset<<std::endl;
+  lock2.unlock();
+}
 
 void download_file(int file_id) {
   std::unique_lock<std::mutex> lock(dlock);
     int dtime = downTime(mt);  
-    std::cout<<"Downloading File "<<file_id<<"... : ("<<dtime<<" seconds)"<<std::endl;
+    std::cout<<yellow<<"Downloading File "<<file_id<<"... : ("<<dtime<<" seconds)"<<reset<<std::endl;
   lock.unlock();
 
-    //std::cout<<"\n";
     std::this_thread::sleep_for(std::chrono::seconds(dtime));
 
       
   std::unique_lock<std::mutex> lock2(slock);
-    std::cout<<"File "<<file_id<<" downloaded successfully!"<<std::endl;
+    std::cout<<green<<"File "<<file_id<<" downloaded successfully!"<<reset<<std::endl;
   lock2.unlock();
-  //std::cout<<"\n";
+
+  process_file(file_id);
 }
 
 
