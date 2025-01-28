53d4243a033e9def82f2d34c07c9049261f76b08
HarshF40 <harshgaonkar57@gmail.com>
2025-01-28 23:54:03 +0530

    KMP day 3 : (

--- dsa/Algorithms/kmpPatternMatch/kmp.cpp
+++ dsa/Algorithms/kmpPatternMatch/kmp.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
-std::vector<int> strtstr(std::string str, std::string needle){
+#include <cstring>
+std::vector<int> strstr(std::string str, std::string needle){
 	std::vector<int> idx;
 	if (needle.empty()) {
 		for(size_t i = 0; i < str.size(); i++ ){
@@ -7,40 +8,48 @@ std::vector<int> strtstr(std::string str, std::string needle){
 		}
 		return idx;
 	}
-	std::vector<int> lps(needle.length(), 0);
-	size_t prevLPS = 1; //index pointer
+	//Note: 0th postion will always have 0 matching prefix suffix as a single char canot have suffix or prefix
+	std::vector<int> lps(needle.length(), 0); //lps holds the number of characters matching in the string, at each position on i in the pattern
+	size_t prevLPS = 0; //index pointer
 	size_t i = 1;
 	//creating LPS array
 	while(i < needle.size()){
-		if(needle[i] == needle[prevLPS]){
+		if(needle[i] == needle[prevLPS]){ //check if the character is same as the next character
 			lps[i] = prevLPS + 1;
 			prevLPS++;
 			i++;
-		} else if(prevLPS == 0){
+		} else if(prevLPS == 0){ //if character is not same as the character as next character and, prevLPS is 0, then lps[i] will also be zero as there are no matching charcters, and you **cant even move back** to see if there are any other matching characters as prevLPS is at the start of the string, 
 				lps[i] = 0;
 				i+=1;
-	        } else {
+	        } else { //!!!
 				prevLPS = lps[prevLPS - 1];
 			}
 		}
-
 	i = 0;
 	int j = 0;
-	//actual pattern matching
-	while(i < str.size()){
-		if(str[i] == needle[j]){
+	while(i < str.length()){
+		if(str[i] == needle[i]){
 			i++;
 			j++;
 		} else {
 			if(j == 0){
 				i++;
 			} else {
-			j = lps[j - 1];
+				j = lps[j - 1];
+			}
+		}
+		if(j == needle.length()){
+			idx.push_back(i - needle.length());
 		}
 	}
+	return idx;
+}
 
-	}
-	if(j == needle.length()){
-			idx.push_back();
-
+int main(){
+	std::string str = "ABAXABCABC";
+	std::string needle = "ABC";
+	std::vector<int> idx = strstr(str, needle);
+	for(auto const elem : idx)
+		std::cout<<elem;
+	return 0;
 }
--- dsa/problems/patternMatchingString/pattern.cpp
+++ dsa/problems/patternMatchingString/pattern.cpp
@@ -10,12 +10,9 @@ int main(void){
 	std::cin>>pattern;
 
 	for(size_t i = 0; i < str.length() - pattern.length() + 1;){
-		std::cout<<i<<std::endl;
 		if(pattern == str.substr(i, pattern.length())){
 			indexes.push_back(i);
 			i += pattern.length();
-//		} else if(pattern[0] == str.substr(i + 1, str.length() - i)[str.substr(i + 1, str.length() - i).find(pattern[0]) + 1]){ //come back later
-//			i = str.substr(i + 1, str.length() - i).find(pattern[0]);
 		} else {
 			i++;
 		}
