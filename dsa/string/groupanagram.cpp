d90734ed9b892e07b804c60f383f61087706c934
HarshF40 <harshgaonkar57@gmail.com>
2025-02-03 20:12:26 +0530

    group anagram complete

--- dsa/string/groupanagram.cpp
+++ dsa/string/groupanagram.cpp
@@ -2,35 +2,43 @@
 #include<string>
 #include<vector>
 
-std::vector<std::vector<std::string>> quickSort(std::vector<std::string>&);
-void qs(std::string&, int, int);
-int partition(std::string&, int, int);
+template <typename T>
+void qs(T&, int, int);
+
+template <typename T>
+int partition(T&, int, int);
+
+void quickSort(std::vector<std::string>&);
 std::vector<std::vector<std::string>> sep(std::vector<std::string>);
+std::vector<std::vector<std::string>> quickSortM_(std::vector<std::string>&);
 
 int main(){
 	std::vector<std::string> words = {"cat", "act", "dog", "god", "tac", "bat"};
 	std::vector<std::vector<std::string>> solution;
-	solution = quickSort(words);
-	for(const auto& element: words)
-		std::cout<<element<<" ";
-	std::cout<<std::endl;
+	
+	quickSort(words); //sorts each string in the array
+	solution = quickSortM_(words); //sorts the the whole array
+
+	//display the array
 	for(const auto& row : solution){
-		for(const auto& element : row)
-			std::cout<<element;
-		std::cout<<"\n";
+		for(const auto& element : row){ std::cout<<element<<" "; }
+		std::cout<<std::endl;
 	}
-	std::cout<<std::endl;
 	return 0;
 }
+/////////////////////////////////////////////////////////////////////////////////////
+//Quick Sort algorithm
+void quickSort(std::vector<std::string>& words){ 
+	for(size_t i = 0; i < words.size(); i++) qs(words[i], 0, words[i].size() - 1); 
+}
 
-std::vector<std::vector<std::string>> quickSort(std::vector<std::string>& words){
-	for(size_t i = 0; i < words.size(); i++){
-		qs(words[i], 0, words[i].size() - 1);
-	}
+std::vector<std::vector<std::string>> quickSortM_(std::vector<std::string>& words){
+	qs(words, 0, words.size() - 1);
 	return sep(words);
 }
 
-void qs(std::string& word, int low, int high){
+template <typename T>
+void qs(T& word, int low, int high){
 	if(low < high){
 		int partitionIndex = partition(word, low, high);
 		qs(word, low, partitionIndex - 1);
@@ -38,19 +46,21 @@ void qs(std::string& word, int low, int high){
 	}
 }
 
-int partition(std::string& word, int low, int high){
+template <typename T>
+int partition(T& word, int low, int high){
 	int i = low;
 	int j = high;
-	char pivot = word[low];
+	auto pivot = word[low];
 	while(i < j){
 		while(pivot >= word[i] && i < high) i++;
 		while(pivot < word[j] && j > low) j--;
-		if(i < j) std::swap<char>(word[i], word[j]);
+		if(i < j) std::swap(word[i], word[j]);
 	}
-	std::swap<char>(word[j], word[low]);
+	std::swap(word[j], word[low]);
 	return j;
 }
-
+/////////////////////////////////////////////////////////////////////////////////////
+//seggregate the arrays
 std::vector<std::vector<std::string>> sep(std::vector<std::string> words){
 	std::vector<std::vector<std::string>> solution;
 	size_t prev = 0;
