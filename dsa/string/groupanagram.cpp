5f8c4985bbc6691a62f2bd75fd96101aaf2065d8
HarshF40 <harshgaonkar57@gmail.com>
2025-02-03 13:10:02 +0530

    group anagaram

--- dsa/string/groupanagram.cpp
+++ dsa/string/groupanagram.cpp
@@ -1,18 +1,25 @@
 #include<iostream>
 #include<string>
-#include <type_traits>
 #include<vector>
 
-std:: quickSort(std::vector<std::string>&);
+std::vector<std::vector<std::string>> quickSort(std::vector<std::string>&);
 void qs(std::string&, int, int);
 int partition(std::string&, int, int);
+std::vector<std::vector<std::string>> sep(std::vector<std::string>);
 
 int main(){
 	std::vector<std::string> words = {"cat", "act", "dog", "god", "tac", "bat"};
 	std::vector<std::vector<std::string>> solution;
-	quickSort(words);
+	solution = quickSort(words);
 	for(const auto& element: words)
 		std::cout<<element<<" ";
+	std::cout<<std::endl;
+	for(const auto& row : solution){
+		for(const auto& element : row)
+			std::cout<<element;
+		std::cout<<"\n";
+	}
+	std::cout<<std::endl;
 	return 0;
 }
 
@@ -42,4 +49,25 @@ int partition(std::string& word, int low, int high){
 	}
 	std::swap<char>(word[j], word[low]);
 	return j;
+}
+
+std::vector<std::vector<std::string>> sep(std::vector<std::string> words){
+	std::vector<std::vector<std::string>> solution;
+	size_t prev = 0;
+	size_t curr = 0;
+	while(curr < words.size()){
+		std::vector<std::string> temp;
+		if(prev == curr){
+			temp.push_back(words[curr]);
+			curr++;
+		} else if(words[prev] == words[curr]){
+			temp.push_back(words[curr]);
+			curr++;
+			prev++;
+		} else {
+			prev = curr;
+		}
+		solution.push_back(temp);
+	}
+	return solution;
 }
